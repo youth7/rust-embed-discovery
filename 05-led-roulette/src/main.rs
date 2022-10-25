357c1d89417f4493b525e122c2c9077f2443541d
chris huang <youth7@163.com>
2022-10-25 11:45:04 +0800

    初始化

---
+++ 05-led-roulette/src/main.rs
@@ -0,0 +1,16 @@
+#![deny(unsafe_code)]
+#![no_main]
+#![no_std]
+
+use cortex_m_rt::entry;//这个和《The embedonomicon》中的entry宏原理是一样的，原理是库函数才是真正的入口，然后库函数再来加载用户函数
+use panic_halt as _;
+use microbit as _;
+
+#[entry]
+fn main() -> ! {
+    let _y;
+    let x = 42;
+    _y = x;
+    // infinite loop; just so we don't leave this stack frame
+    loop {}
+}
