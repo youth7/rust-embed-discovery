eb773071ab1e1ca5ab69b68195eb19e66682e48b
chris huang <youth7@163.com>
2022-10-27 15:33:21 +0800

    初步完成flash相关代码

--- 05-led-roulette/src/main.rs
+++ 05-led-roulette/src/main.rs
@@ -4,10 +4,9 @@
 
 use cortex_m_rt::entry;//这个和《The embedonomicon》中的entry宏原理是一样的，原理是库函数才是真正的入口，然后库函数再来加载用户函数
 use panic_halt as _;
-use microbit as _;
 
 #[entry]
-fn main() -> ! {
+fn main() -> ! {//发散函数，《The embedonomicon》也说过
     let _y;
     let x = 42;
     _y = x;
