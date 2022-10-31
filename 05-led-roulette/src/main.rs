b7b49ba3625eb88aa7c61efa44c11f87a2861c4f
chris huang <youth7@163.com>
2022-10-31 17:54:04 +0800

    完成lighting up的代码

--- 05-led-roulette/src/main.rs
+++ 05-led-roulette/src/main.rs
@@ -4,12 +4,14 @@
 
 use cortex_m_rt::entry;//这个和《The embedonomicon》中的entry宏原理是一样的，原理是库函数才是真正的入口，然后库函数再来加载用户函数
 use panic_halt as _;
+use microbit::board::Board;
+use microbit::hal::prelude::*;
 
 #[entry]
 fn main() -> ! {//发散函数，《The embedonomicon》也说过
-    let _y;
-    let x = 42;
-    _y = x;
+    let mut board = Board::take().unwrap();
+    board.display_pins.col1.set_low().unwrap();
+    board.display_pins.row1.set_high().unwrap();
     // infinite loop; just so we don't leave this stack frame
     loop {}
 }
