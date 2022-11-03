00195ec9cb53384edee989e2d86df2c959c89695
chris huang <youth7@163.com>
2022-11-03 15:47:42 +0800

    完成led轮盘最终内容

--- 05-led-roulette/src/main.rs
+++ 05-led-roulette/src/main.rs
@@ -2,21 +2,56 @@
 #![no_main]
 #![no_std]
 
-use cortex_m_rt::entry;//这个和《The embedonomicon》中的entry宏原理是一样的，原理是库函数才是真正的入口，然后库函数再来加载用户函数
+use cortex_m_rt::entry; //这个和《The embedonomicon》中的entry宏原理是一样的，原理是库函数才是真正的入口，然后库函数再来加载用户函数
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 use panic_halt as _;
-use microbit::board::Board;
-use microbit::hal::prelude::*;
 
 #[entry]
-fn main() -> ! {//发散函数，《The embedonomicon》也说过
-    let mut board = Board::take().unwrap();
-    // 这个api控制的是整行/列的pin的状态，而一个灯点亮的条件是对应的pin上同时有高低两种状态
-    // 例如以下代码会点亮四个角的led
-    board.display_pins.col1.set_low().unwrap();
-    board.display_pins.row1.set_high().unwrap();
+fn main() -> ! {
+    //发散函数，《The embedonomicon》也说过
 
-    board.display_pins.col5.set_low().unwrap();
-    board.display_pins.row5.set_high().unwrap();
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let circle2 = [
+        // 外圈全亮
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+    ];
+    let circle1 = [
+        // 中间圈全亮
+        [0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 1, 0, 1, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 0],
+    ];
+    let circle0 = [
+        // 里圈全亮
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    let light_up_duration = 120;
+
+    loop {
+        display.show(&mut timer, circle0, light_up_duration);
+        display.clear();
+        display.show(&mut timer, circle1, light_up_duration);
+        display.clear();
+        display.show(&mut timer, circle2, light_up_duration);
+        display.clear();
+        timer.delay_ms(500u16);
+
+    }
 }
