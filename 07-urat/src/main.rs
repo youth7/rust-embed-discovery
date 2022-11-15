a19199af2d174e558aaf8e1b0f19431a60053691
chris huang <youth7@163.com>
2022-11-15 11:35:18 +0800

    改造为直接调用的方式

--- 07-urat/src/main.rs
+++ 07-urat/src/main.rs
@@ -1,36 +1,34 @@
 #![no_main]
 #![no_std]
 
-use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
+use cortex_m_rt::entry; // cortex-m的运行时
 use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
 
 use microbit::{
     hal::prelude::*,
-    hal::uarte,
-    hal::uarte::{Baudrate, Parity},
+    hal::uarte::{Baudrate, Parity, Uarte},
 };
 
-mod serial_setup;
-use serial_setup::UartePort;
+static mut TX_BUF: [u8; 1] = [0; 1];
+static mut RX_BUF: [u8; 1] = [0; 1];
 
 #[entry]
 fn main() -> ! {
-    rtt_init_print!();
+    rtt_init_print!(); //初始化rtt
     let board = microbit::Board::take().unwrap();
-
-    let mut serial = {
-        let serial = uarte::Uarte::new(
-            board.UARTE0,
-            board.uart.into(),
-            Parity::EXCLUDED,
-            Baudrate::BAUD115200,
-        );
-        UartePort::new(serial)
-    };
-
-    nb::block!(serial.write(b'X')).unwrap();
-    nb::block!(serial.flush()).unwrap();
+    let uarte_instance = Uarte::new(//构造一个uarte实例
+        board.UARTE0,
+        board.uart.into(),
+        Parity::EXCLUDED,
+        Baudrate::BAUD115200,
+    );
+    //将uarte实例上的tx和rx提取出来单独使用
+    let (mut tx, rx) = uarte_instance
+        .split(unsafe { &mut TX_BUF }, unsafe { &mut RX_BUF })
+        .unwrap();
+    nb::block!(tx.write(b'X')).unwrap(); //往串口上写入字符X
+    nb::block!(tx.flush()).unwrap(); //强制清空缓冲区，立即写入
 
     loop {}
 }
