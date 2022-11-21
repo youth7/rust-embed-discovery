e12653aad3879fda543eb1bcf86510d91d85e7f1
chris huang <youth7@163.com>
2022-11-21 21:05:21 +0800

    完成写入字符串

--- 07-urat/src/main.rs
+++ 07-urat/src/main.rs
@@ -1,6 +1,8 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
+
 use cortex_m_rt::entry; // cortex-m的运行时
 use panic_rtt_target as _;
 use rtt_target::rtt_init_print;
@@ -17,18 +19,22 @@ static mut RX_BUF: [u8; 1] = [0; 1];
 fn main() -> ! {
     rtt_init_print!(); //初始化rtt
     let board = microbit::Board::take().unwrap();
-    let uarte_instance = Uarte::new(//构造一个uarte实例
+    let uarte_instance = Uarte::new(
+        //构造一个uarte实例
         board.UARTE0,
         board.uart.into(),
         Parity::EXCLUDED,
         Baudrate::BAUD115200,
     );
     //将uarte实例上的tx和rx提取出来单独使用
-    let (mut tx, rx) = uarte_instance
+    let (mut tx, _rx) = uarte_instance
         .split(unsafe { &mut TX_BUF }, unsafe { &mut RX_BUF })
         .unwrap();
-    nb::block!(tx.write(b'X')).unwrap(); //往串口上写入字符X
-    nb::block!(tx.flush()).unwrap(); //强制清空缓冲区，立即写入
 
+    for byte in b"\xE9\x9B\xB7\xE7\x8C\xB4\r\n".iter() {
+        nb::block!(tx.write(*byte)).unwrap(); //往串口上写入字符X
+    }
+    
+    tx.write_str("The quick brown fox jumps over the lazy dog.\r\n\n").unwrap();
     loop {}
 }
