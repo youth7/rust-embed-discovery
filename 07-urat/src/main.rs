033df1932ded31ca2a5e6c79b9fd02ef57a09330
chris huang <youth7@163.com>
2022-11-24 20:32:59 +0800

    完成echo服务器功能

--- 07-urat/src/main.rs
+++ 07-urat/src/main.rs
@@ -1,15 +1,17 @@
 #![no_main]
 #![no_std]
 
-use core::fmt::Write;
 
+use core::fmt::Write;
+use core::str;
 use cortex_m_rt::entry; // cortex-m的运行时
 use panic_rtt_target as _;
-use rtt_target::rtt_init_print;
+use rtt_target::{rtt_init_print, rprintln};
+use heapless::Vec;
 
 use microbit::{
     hal::prelude::*,
-    hal::uarte::{Baudrate, Parity, Uarte},
+    hal::uarte::{Baudrate, Parity, Uarte, UarteTx}, pac::{UARTE0},
 };
 
 static mut TX_BUF: [u8; 1] = [0; 1];
@@ -27,14 +29,31 @@ fn main() -> ! {
         Baudrate::BAUD115200,
     );
     //将uarte实例上的tx和rx提取出来单独使用
-    let (mut tx, _rx) = uarte_instance
+    let (mut tx, mut rx) = uarte_instance
         .split(unsafe { &mut TX_BUF }, unsafe { &mut RX_BUF })
         .unwrap();
-
-    for byte in b"\xE9\x9B\xB7\xE7\x8C\xB4\r\n".iter() {
-        nb::block!(tx.write(*byte)).unwrap(); //往串口上写入字符X
+    let mut buffer:Vec<u8, 32> = Vec::new();    
+    loop {
+        //read是非阻塞的，如果没有字符可读的时候会抛出一个异常，
+        //为了能够读取到数据需要持续地调用read直到不抛异常为止，而nb::block其实就是做了这个事情
+        let byte = nb::block!(rx.read()).unwrap();
+        if byte == 0x0D {
+            send_str(&mut tx, &mut buffer);
+        }else{
+            if buffer.push(byte).is_err() {
+                send_str(&mut tx, &mut buffer);
+                continue;
+            }
+        }
     }
-    
-    tx.write_str("The quick brown fox jumps over the lazy dog.\r\n\n").unwrap();
-    loop {}
+}
+
+fn send_str(tx: &mut UarteTx<UARTE0>, buffer: &mut Vec<u8, 32>){
+    // buffer.reverse();
+    let content = str::from_utf8(&buffer).unwrap();
+    rprintln!("{}", content);
+    tx.write_str(content).unwrap();
+    tx.write_str("\r\n").unwrap();
+    nb::block!(tx.flush()).unwrap();
+    buffer.clear();
 }
