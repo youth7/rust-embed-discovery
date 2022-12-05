9cea0e460391adbce1e0e063e28d1750273567d3
chris huang <youth7@163.com>
2022-12-05 20:34:01 +0800

    增加写寄存器一节的描述

--- 08-i2c/src/main.rs
+++ 08-i2c/src/main.rs
@@ -5,32 +5,35 @@ use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
 
+use microbit::hal::prelude::*;
+
 use microbit::{
     hal::twim::Twim,
     pac::twim0::frequency::FREQUENCY_A,
 };
 
-use lsm303agr::{
-    AccelOutputDataRate, Lsm303agr,
-};
+const ACCELEROMETER_ADDR: u8 = 0b0011001;//加速计地址
+const MAGNETOMETER_ADDR: u8 = 0b0011110;//磁力计地址
 
+const ACCELEROMETER_ID_REG: u8 = 0x0f;//加速计的寄存器地址
+const MAGNETOMETER_ID_REG: u8 = 0x4f;//磁力计的寄存器地址
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
-    let i2c =  Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) ;//构造一个twim对象，它兼容i2c
+    let mut i2c =  Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) ;//构造一个twim对象，它兼容i2c
 
-    // Code from documentation
-    let mut sensor = Lsm303agr::new_with_i2c(i2c);
-    sensor.init().unwrap();
-    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
-    loop {
-        if sensor.accel_status().unwrap().xyz_new_data {
-            let data = sensor.accel_data().unwrap();
-            // RTT instead of normal print
-            rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
-        }
-    }
+    let mut acc = [0];//缓存，用来存放i2c设备的响应信息，这种通风格式和上一章uart不同，是因为i2c的半双工通讯
+    let mut mag = [0];
+
+    // 向设备的寄存器写入信息，然后读取设备的响应并写入到缓存中
+    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
+    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
+
+    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
+    rprintln!("The magnetometer  chip's id is: {:#b}", mag[0]);
+
+    loop {}
 }
