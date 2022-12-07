622892357bf5b38746206f2264a9b93326199cb8
chris huang <youth7@163.com>
2022-12-07 19:10:43 +0800

    初步跑通流程

--- 08-i2c/src/main.rs
+++ 08-i2c/src/main.rs
@@ -1,39 +1,101 @@
 #![no_main]
 #![no_std]
+use core::fmt::Write;
+use core::str;
+use heapless::Vec;
 
 use cortex_m_rt::entry;
-use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
-
-use microbit::hal::prelude::*;
+use rtt_target::{rprintln, rtt_init_print};
 
 use microbit::{
-    hal::twim::Twim,
+    hal::prelude::*,
+    hal::uarte::{Baudrate, Parity, Pins, Uarte, UarteTx},
+    hal::{twim::Twim, uarte::UarteRx},
     pac::twim0::frequency::FREQUENCY_A,
+    pac::{TWIM0, UARTE0},
 };
 
-const ACCELEROMETER_ADDR: u8 = 0b0011001;//加速计地址
-const MAGNETOMETER_ADDR: u8 = 0b0011110;//磁力计地址
+use lsm303agr::{
+    interface::I2cInterface, mode::MagOneShot, AccelOutputDataRate, Lsm303agr, MagOutputDataRate,
+};
 
-const ACCELEROMETER_ID_REG: u8 = 0x0f;//加速计的寄存器地址
-const MAGNETOMETER_ID_REG: u8 = 0x4f;//磁力计的寄存器地址
+static mut TX_BUF: [u8; 1] = [0; 1];
+static mut RX_BUF: [u8; 1] = [0; 1];
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
+    //初始化uart设备
+    let (mut tx, mut rx) = get_tx_and_rx(
+        board.UARTE0,
+        board.uart.into(),
+        Parity::EXCLUDED,
+        Baudrate::BAUD115200,
+    );
+    //初始化i2c设备
+    let i2c = Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100);
+    let mut sensor = init_sensor(i2c);
+    let mut buffer: Vec<u8, 32> = Vec::new();
+    loop {
+        //跟i2c通讯
+        //回显读取到的内容
 
-    let mut i2c =  Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) ;//构造一个twim对象，它兼容i2c
+        //阻塞读取uart设备
+        let byte = nb::block!(rx.read()).unwrap();
+        if byte == 0x0D {
+            //判断读取到的内容
+            let input = core::str::from_utf8(&buffer).unwrap();
+            rprintln!("a:{}", input);
+            match input {
+                "a" => {
+                    if sensor.accel_status().unwrap().xyz_new_data {
+                        let data = sensor.accel_data().unwrap();
+                        rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
+                    }
+                }
+                "m" => {
+                    if sensor.mag_status().unwrap().x_new_data {
+                        let data = sensor.mag_data().unwrap();
+                        rprintln!("Magnetometer: x {} y {} z {}", data.x, data.y, data.z);
+                    }
+                },
+                _ => send_error(&mut tx, input),
+            }
+            buffer.clear();
+            continue;
+        }
+        if buffer.push(byte).is_err() {
+            send_error(&mut tx, "输入过长");
+            buffer.clear();
+        }
+    }
+}
 
-    let mut acc = [0];//缓存，用来存放i2c设备的响应信息，这种通风格式和上一章uart不同，是因为i2c的半双工通讯
-    let mut mag = [0];
+fn get_tx_and_rx(
+    uarte: UARTE0,
+    pins: Pins,
+    parity: Parity,
+    baudrate: Baudrate,
+) -> (UarteTx<UARTE0>, UarteRx<UARTE0>) {
+    let uarte_instance = Uarte::new(uarte, pins, Parity::EXCLUDED, Baudrate::BAUD115200);
+    uarte_instance
+        .split(unsafe { &mut TX_BUF }, unsafe { &mut RX_BUF })
+        .unwrap()
+}
 
-    // 向设备的寄存器写入信息，然后读取设备的响应并写入到缓存中
-    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
-    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
+fn init_sensor(i2c: Twim<TWIM0>) -> Lsm303agr<I2cInterface<Twim<TWIM0>>, MagOneShot> {
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz50).unwrap();
+    sensor.set_mag_odr(MagOutputDataRate::Hz50).unwrap();
+    sensor
+}
 
-    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-    rprintln!("The magnetometer  chip's id is: {:#b}", mag[0]);
 
-    loop {}
+fn send_error(tx: &mut UarteTx<UARTE0>, error_message: &str) {
+    tx.write_str(error_message).unwrap();
+    tx.write_str("\r\n").unwrap(); //加上回车换行以便显示
+    nb::block!(tx.flush()).unwrap(); //刷新缓冲区以免内容残留
 }
